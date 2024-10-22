a97aa06b1ccd94aaeb87fcbcc6e519b49e41cdb3
VineeTagarwaL-code <vineetagarwal.now@gmail.com>
2024-10-23 00:11:26 +0530

    [chore]: merged master to feat-be

--- apps/server/src/controllers/order/index.ts
+++ apps/server/src/controllers/order/index.ts
@@ -2,18 +2,22 @@ import { addToOrderQueue } from "@repo/order-queue";
 import { AsyncWrapper } from "../../utils/asynCatch";
 import { generateOrderId } from "../../utils/utils";
 import { SuccessResponse } from "../../utils/wrappers/success.res";
-import { EOrderType } from "@opinix/types";
+import { sides } from "@opinix/types";
 import { Request } from "express";
 import { RedisManager } from "@repo/order-queue";
+import { ErrorHandler } from "../../utils/wrappers/error.res";
+import { CREATE_ORDER } from "@opinix/types";
+import { MessageFromApi } from "@opinix/types";
 
-let redisClient = RedisManager.getInstance();
-type TPlaceOrder = {
+type TPlaceOrderReq = {
   event_id: number;
   l1_expected_price: number;
   l1_order_quantity: number;
-  offer_type: EOrderType;
+  offer_type: sides;
 };
+
 export const placeHandler = AsyncWrapper(
+<<<<<<< Updated upstream
   async (req: Request<{}, {}, TPlaceOrder>, res) => {
     const { event_id, l1_expected_price, l1_order_quantity, offer_type } = req.body;
 
@@ -30,6 +34,31 @@ export const placeHandler = AsyncWrapper(
     };
     await addToOrderQueue(order);
     redisClient.publishMessage(orderId, order);
+=======
+  async (req: Request<{}, {}, TPlaceOrderReq>, res) => {
+    const { event_id, l1_expected_price, l1_order_quantity, offer_type } =
+      req.body;
+
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new ErrorHandler("Unauthorized", "AUTHENTICATION_FAILED");
+    }
+
+    // TODO: decode the token and get the number for now hardcoded
+    let userid = 1;
+    const data: MessageFromApi = {
+      type: CREATE_ORDER,
+      data: {
+        market: event_id.toString(),
+        price: l1_expected_price,
+        quantity: l1_order_quantity,
+        side: offer_type,
+        userId: userid.toString(),
+      },
+    };
+
+    await addToOrderQueue(data);
+>>>>>>> Stashed changes
     let response = new SuccessResponse("Order placed successfully", 201);
     return res.status(201).json(response);
   }
