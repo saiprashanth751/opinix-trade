62baa25648da2fa2fed936b0f90fc5a88bd1dbaa
Prashant Varma <prashantvarma5083@gmail.com>
2024-11-04 20:27:53 +0530

    fix: create random eventID

--- services/engine/src/index.ts
+++ services/engine/src/index.ts
@@ -2,16 +2,15 @@
  TODOS:
  1. Fake Liquidity []
  2. setting base balancing [x]
- 3. go through again 
- 4. TEST ENGINE ( Latency ) | shift it to RUST or GO if needed.
- 5. TYPES
- 6. Test Cases
+ 3. go through again []
+ 4. TEST ENGINE ( Latency ) | shift it to RUST or GO if needed.[]
+ 5. TYPES [x]
+ 6. Test Cases [x]
 */
 
 import { RedisManager } from "@repo/order-queue";
 import { Engine } from "./trade/Engine";
-import { Orderbook } from "./trade/Orderbook"
-
+import { Orderbook } from "./trade/Orderbook";
 
 async function main() {
     const engine = new Engine();
@@ -20,28 +19,27 @@ async function main() {
     console.log("connected to redis");
 
     while (true) {
-        const response = await redisClient.rPop("ORDER_QUEUE")
-        const res = JSON.parse(response!)
+        const response = await redisClient.rPop("ORDER_QUEUE");
+        const parsedRespnse = JSON.parse(response!);
         if (!response) {
-        } else {
-            const t = res.type;
-            const mess = res.data;
-            if (mess.side === "buy") {
-                mess.side = "yes";
+
+        }else {
+            const type = parsedRespnse.type;
+            const responseMessage = parsedRespnse.data;
+            if (responseMessage.side === "buy") {
+                responseMessage.side = "yes";
             } else {
-                mess.side = "no";
+                responseMessage.side = "no";
             }
             const message = {
-                type: mess.type,
-                data: mess
-            }
-            engine.processOrders({ message, clientId: t });
+                type: responseMessage.type,
+                data: responseMessage,
+            };
+            engine.processOrders({ message, clientId: type });
         }
     }
-
 }
 
 main();
-
 
 export { Engine, Orderbook };
