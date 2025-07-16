fae7ff26c73d8d3eb8aea8192cf3fac22d8bd09d
Sai Prashanth <saiprashanth751@gmail.com>
2025-07-16 10:21:25 +0530

    Updated orderRouter.ts

--- apps/server/src/router/orderRouter.ts
+++ apps/server/src/router/orderRouter.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { initiateOrderValidator } from "../middlewares/validators/initiate.validator";
 import { placeHandler } from "../controllers/order";
+
 const orderRouter = Router();
+
 orderRouter.post("/initiate", initiateOrderValidator, placeHandler);
 export { orderRouter };
